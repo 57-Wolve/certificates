a769d6bba5dccf3ade956650f3e447c181d51c97
William Gill <william.gill@anomalous.dev>
2022-07-25 22:57:16 +0000

    PKI Policy

--- pki/pki.go
+++ pki/pki.go
@@ -515,24 +515,15 @@ func (p *PKI) GenerateRootCertificate(name, org, resource string, pass []byte) (
 			},
 			PolicyIdentifiers: []asn1.ObjectIdentifier{
 				[]int{2, 5, 29, 32, 0},			// All Issuance Policies
-				[]int{2, 23, 140, 1, 1},		// Certificate issued in compliance with the Extended Validation Guidelines (2.23.140.1.1)
-				[]int{2, 23, 140, 1, 3},		// EV Code Signing Certificate issued in compliance with the Code Signing Baseline Requirements (2.23.140.1.3)
+				//[]int{2, 23, 140, 1, 1},		// Certificate issued in compliance with the Extended Validation Guidelines (2.23.140.1.1)
+				//[]int{2, 23, 140, 1, 3},		// EV Code Signing Certificate issued in compliance with the Code Signing Baseline Requirements (2.23.140.1.3)
 				//[]int{2, 23, 140, 1, 2, 1},	// Certificate issued in compliance with the TLS Baseline Requirements – No entity identity asserted (2.23.140.1.2.1)
 				//[]int{2, 23, 140, 1, 2, 2},	// Certificate issued in compliance with the TLS Baseline Requirements – Organization identity asserted (2.23.140.1.2.2)
 				//[]int{2, 23, 140, 1, 2, 3},	// Certificate issued in compliance with the TLS Baseline Requirements – Individual identity asserted (2.23.140.1.2.3)
 			},
-			KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment | x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+			KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
 			ExtKeyUsage: []x509.ExtKeyUsage{
-				x509.ExtKeyUsageServerAuth,
-				x509.ExtKeyUsageClientAuth,
-				x509.ExtKeyUsageCodeSigning,
-				x509.ExtKeyUsageEmailProtection,
-				x509.ExtKeyUsageTimeStamping,
-			},
-			UnknownExtKeyUsage: []asn1.ObjectIdentifier{
-				[]int{1, 3, 6, 1, 4, 1, 311, 20, 2, 2},		// Smart Card Logon (1.3.6.1.4.1.311.20.2.2)
-				[]int{1, 3, 6, 1, 4, 1, 311, 10, 3, 12},	// Document Signing (1.3.6.1.4.1.311.10.3.12)
-				[]int{1, 3, 6, 1, 4, 1, 311, 80, 1},		// Document Encryption (1.3.6.1.4.1.311.80.1)
+				x509.ExtKeyUsageAll,
 			},
 			BasicConstraintsValid: true,
 			IsCA:                  true,
@@ -596,13 +587,13 @@ func (p *PKI) GenerateIntermediateCertificate(name, org, resource string, parent
 			},
 			PolicyIdentifiers: []asn1.ObjectIdentifier{
 				[]int{2, 5, 29, 32, 0},			// All Issuance Policies
-				[]int{2, 23, 140, 1, 1},		// Certificate issued in compliance with the Extended Validation Guidelines (2.23.140.1.1)
-				[]int{2, 23, 140, 1, 3},		// EV Code Signing Certificate issued in compliance with the Code Signing Baseline Requirements (2.23.140.1.3)
+				//[]int{2, 23, 140, 1, 1},		// Certificate issued in compliance with the Extended Validation Guidelines (2.23.140.1.1)
+				//[]int{2, 23, 140, 1, 3},		// EV Code Signing Certificate issued in compliance with the Code Signing Baseline Requirements (2.23.140.1.3)
 				//[]int{2, 23, 140, 1, 2, 1},	// Certificate issued in compliance with the TLS Baseline Requirements – No entity identity asserted (2.23.140.1.2.1)
 				//[]int{2, 23, 140, 1, 2, 2},	// Certificate issued in compliance with the TLS Baseline Requirements – Organization identity asserted (2.23.140.1.2.2)
 				//[]int{2, 23, 140, 1, 2, 3},	// Certificate issued in compliance with the TLS Baseline Requirements – Individual identity asserted (2.23.140.1.2.3)
 			},
-			KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment | x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
+			KeyUsage: x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
 			ExtKeyUsage: []x509.ExtKeyUsage{
 				x509.ExtKeyUsageServerAuth,
 				x509.ExtKeyUsageClientAuth,
